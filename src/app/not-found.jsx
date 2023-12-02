2a58c5b314608df7881e3d091f48b600f93b0e4f
galangriarulhaq <galangrz337@gmail.com>
2023-12-03 06:31:31 +0700

    Update file not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,16 +2,19 @@
 
 import { FileSearch } from "@phosphor-icons/react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+
   return (
     <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
       <div className="flex justify-center items-center gap-4 flex-col">
         <FileSearch size={42} className="text-zinc-50" />
         <h3 className="text-zinc-50 text-4xl font-bold">NOT FOUND</h3>
-        <Link href="/" className="text-zinc-50 hover:text-blue-700 transition-all underline">
+        <button onClick={() => router.back()} className="text-zinc-50 hover:text-blue-700 transition-all underline">
           Back...
-        </Link>
+        </button>
       </div>
     </div>
   );
