e00fdd6b4f539275021798f02a96a342a371902b
galangriarulhaq <galangrz337@gmail.com>
2023-12-05 00:12:57 +0700

    Fix problem in the collection

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,36 +1,29 @@
 import Header from "@/components/Dashboard/Header";
+import { authUserSession } from "@/libs/auth";
+import prisma from "@/libs/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
-const Page = () => {
+const Page = async () => {
+  const user = await authUserSession();
+  const collection = await prisma.collection.findMany({
+    where: { user_email: user.email },
+  });
+
   return (
     <section className="mt-4 px-4 w-full">
       <Header title={"My Collection"} />
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <Link href="/" className="relative border-2 border-violet-950">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="flex justify-center items-center absolute bottom-0 w-full bg-violet-950 h-16">
-            <h5 className="text-xl text-center">Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-violet-950">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="flex justify-center items-center absolute bottom-0 w-full bg-violet-950 h-16">
-            <h5 className="text-xl text-center">Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-violet-950">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="flex justify-center items-center absolute bottom-0 w-full bg-violet-950 h-16">
-            <h5 className="text-xl text-center">Judul Anime</h5>
-          </div>
-        </Link>
-        <Link href="/" className="relative border-2 border-violet-950">
-          <Image src="" alt="" width={350} height={350} className="w-full" />
-          <div className="flex justify-center items-center absolute bottom-0 w-full bg-violet-950 h-16">
-            <h5 className="text-xl text-center">Judul Anime</h5>
-          </div>
-        </Link>
+        {collection.map((collect, index) => {
+          return (
+            <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative border-2 border-violet-950">
+              <Image src="" alt="" width={350} height={350} className="w-full" />
+              <div className="flex justify-center items-center absolute bottom-0 w-full bg-violet-950 h-16">
+                <h5 className="text-xl text-center">{collect.anime_mal_id}</h5>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
