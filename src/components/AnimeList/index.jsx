13b673ac3ba4140780463b56ff85331dd103de61
galangriarulhaq <galangrz337@gmail.com>
2024-02-22 21:42:51 +0700

    Styling Anime List

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -6,15 +6,16 @@ const AnimeList = ({ api }) => {
     <div className="grid md:grid-cols-6 sm:grid-cols-3 grid-cols-2 gap-4 px-4 md:px-20">
       {api.data?.map((anime) => {
         return (
-          <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-zinc-50 hover:text-blue-700 transition-all" key={anime.mal_id}>
+          <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-zinc-50 transition-all" key={anime.mal_id}>
             <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className="object-cover" />
-            <div className="bg-gray-700 grid-cols-1 p-2">
-              <h3 className="font-bold text-md mb-2">{anime.title}</h3>
+            <div className="bg-gray-700 grid-cols-1 p-2 hover:text-blue-700 ">
+              <h3 className="font-bold text-md mb-4 flex justify-center items-center">{anime.title}</h3>
               {anime.score && (
-                <div>
+                <div className="grid grid-cols-2 gap-2 px-2">
                   <h4 className="text-sm ">Episode : {anime.episodes}</h4>
                   <h4 className="text-sm ">Score : {anime.score}</h4>
                   <h4 className="text-sm ">Rating : {anime.rating}</h4>
+                  <h4 className="text-sm ">Season : {anime.season}</h4>
                 </div>
               )}
             </div>
